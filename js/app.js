8fda4dc4194cffaa05c4513706517693c9bca418
Camila  lima <92176387+Camilalima84@users.noreply.github.com>
2022-05-13 17:01:47 -0300

    commit inicial

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ const search = document.querySelector("#search")
 
 search.onclick = function(e){
     e.preventDefault()
-    let searchString = document.querySelector("#searchString").Value;
-    let filterType = document.querySelector("input[name='searchfilter']:checked").Value;
+    let searchString = document.querySelector("#searchString").value;
+    let filterType = document.querySelector("input[name='searchfilter']:checked").value;
 
     if(filterType === "all")alert("Busca geral por" + searchString)
     else if (filterType === "cod") alert("Busca pelo código por " + searchString)
