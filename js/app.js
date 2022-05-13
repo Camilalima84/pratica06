5b215d78b85f86de75f93c1d48dce66667078566
Camila  lima <92176387+Camilalima84@users.noreply.github.com>
2022-05-13 16:54:04 -0300

    commit inicial

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
-
+const search = document.querySelector("#search")
 const search = document.querySelector("#pesquisar")
 
-search.onclick = function(){
-    let filters2 = document.querySelector("input[name='searchfilter']:checked").Value;
+search.onclick = function(e){
+    e.preventDefault()
+    let searchString = document.querySelector("#searchString").Value;
+    let filterType = document.querySelector("input[name='searchfilter']:checked").Value;
 
-    alert(filters2)
+    if(filterType === "all")alert("Busca geral por" + searchString)
+    else if (filterType === "cod") alert("Busca pelo código por " + searchString)
+    else if (filterType === "mat") alert("Busca pela matrícula por " + searchString)
+    else alert("Busca pelo nome por " + searchStri
 }
 
